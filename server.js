9c132ccc44a3a150a36104efdb229c0be6491c6e
erikgt12 <maxibolsasmty@gmail.com>
2025-06-19 02:24:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -60,4 +60,7 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log("Server running on port " + port);
+
+  console.log("Mensaje recibido:", incomingMsg);
+console.log("De:", from);
 });
